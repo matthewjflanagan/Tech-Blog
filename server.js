a24b55ed4a33e602d8a201822549d099924c1366
Matthew Flanagan <mattflanagan40@gmail.com>
2021-11-30 19:31:09 -0500

    updated file structure

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 // Import express-handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
+const hbs = exphbs.create({ helpers });
 const path = require('path');
 
 const sequelize = require('./config/connection');
@@ -18,8 +20,9 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(require('./controllers/dish-routes'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Sets up session and connect to our Sequelize db
 const sess = {
@@ -45,8 +48,13 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
