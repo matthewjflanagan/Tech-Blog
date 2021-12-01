086d86a2ac63bf428125f0baecb495dcea19a073
Matthew Flanagan <mattflanagan40@gmail.com>
2021-12-01 08:14:31 -0500

    styling started on homepage, helpers installed

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,10 @@
 module.exports = {
-  format_time: (date) => {
-    return date.toLocaleTimeString();
-  },
   format_date: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
-    }`;
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
   },
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  }
 };
